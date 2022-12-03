b70f014a20c59f53916c279c9de7684808065ad2
MinchoVelev <velev.mincho@gmail.com>
2022-12-03 20:58:36 +0000

    Dec2 part 1

--- solutions/src/main/java/advent2022/Dec2.java
+++ solutions/src/main/java/advent2022/Dec2.java
@@ -11,7 +11,29 @@ public class Dec2 {
 
     public static void main(String[] args) throws IOException {
         try (Scanner scanner = IOUtils.getScanner("Dec2.input", Dec2.class)) {
-            
+            int oponent;
+            int me;
+            int score = 0;
+            while(scanner.hasNext()){
+                String line = scanner.nextLine();
+                oponent = line.charAt(0) - 'A' + 1;
+                me = line.charAt(2) - 'X' + 1;
+                
+                int outcome = me - oponent;
+                score += me;
+
+                if(outcome == 0){
+                    score += 3;
+                }else if(outcome == -2 || outcome == 1){
+                    score += 6;
+                }
+            }
+            System.out.println(score);
+
+
+            //   2 3 1
+            //   1 2 3
+            //   1 1 -2 
         }
     }
     
