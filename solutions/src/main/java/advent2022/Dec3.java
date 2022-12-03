a6cc5d33e65c80940e51f56c557db062101269ce
MinchoVelev <velev.mincho@gmail.com>
2022-12-03 20:42:34 +0000

    fixed naming

---
+++ solutions/src/main/java/advent2022/Dec3.java
@@ -0,0 +1,13 @@
+package advent2022;
+
+import java.util.Scanner;
+
+import advent2022.utils.IOUtils;
+
+public class Dec3 {
+    public static void main(String[] args) {
+        try (Scanner scanner = IOUtils.getScanner("Dec3.input", Dec3.class)) {
+            
+        }
+    }
+}
