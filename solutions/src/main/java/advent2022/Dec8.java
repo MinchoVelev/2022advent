2c6792758d77e01fcf06ba294779a2b91c88bf8e
Mincho Velev <mincho.velev@sap.com>
2022-12-16 15:54:26 +0200

    Dec 8 solution

--- solutions/src/main/java/advent2022/Dec8.java
+++ solutions/src/main/java/advent2022/Dec8.java
@@ -14,25 +14,25 @@ public class Dec8 {
 
         System.out.println("-------------------");
 
-        // System.out.println("Part 2:");
-        // part2();
+        System.out.println("Part 2:");
+        part2();
     }
 
     private static void part1() {
         List<Tree[]> maze = readInput();
 
         int visible = maze.size() * 2 + (maze.get(0).length - 2) * 2;
-        System.out.println(visible);
-        System.out.println(Arrays.asList(maze.get(0)));
+        // System.out.println(visible);
+        // System.out.println(Arrays.asList(maze.get(0)));
 
-        asd: for (int i = 1; i < maze.size() - 1; i++) {
-            for (int j = 1; j < maze.get(0).length - 1; j++) {
+        for (int i = 1; i < maze.size() - 1; i++) {
+            asd: for (int j = 1; j < maze.get(0).length - 1; j++) {
                 Tree currTree = maze.get(i)[j];
 
                 // check left
                 currTree.visible = true; 
                 for (int x = j - 1; x >= 0; x--) {
-                    System.out.printf("Checking %s and comparing %d:%d to %d:%d%n", "LEFT", i, j, i, x);
+                    // System.out.printf("Checking %s and comparing %d:%d to %d:%d%n", "LEFT", i, j, i, x);
                     if (maze.get(i)[x].hight >= currTree.hight) {
                         currTree.visible = false;
                         break;
@@ -46,7 +46,7 @@ public class Dec8 {
                 currTree.visible = true;
                 // check right
                 for (int x = j + 1; x < maze.get(0).length; x++) {
-                    System.out.printf("Checking %s and comparing %d:%d to %d:%d%n", "RIGHT", i, j, i, x);
+                    // System.out.printf("Checking %s and comparing %d:%d to %d:%d%n", "RIGHT", i, j, i, x);
                     if (maze.get(i)[x].hight >= currTree.hight) {
                         currTree.visible = false;
                         break;
@@ -62,7 +62,7 @@ public class Dec8 {
                 currTree.visible = true;
                 // check up
                 for (int y = i - 1; y >= 0; y--) {
-                    System.out.printf("Checking %s and comparing %d:%d to %d:%d%n", "UP", i, j, y, j);
+                    // System.out.printf("Checking %s and comparing %d:%d to %d:%d%n", "UP", i, j, y, j);
                     if (maze.get(y)[j].hight >= currTree.hight) {
                         currTree.visible = false;
                         break;
@@ -77,7 +77,7 @@ public class Dec8 {
                 // check down
                 
                 for (int y = i + 1; y < maze.size(); y++) {
-                    System.out.printf("Checking %s and comparing %d:%d to %d:%d%n", "DOWN", i, j, y, j);
+                    // System.out.printf("Checking %s and comparing %d:%d to %d:%d%n", "DOWN", i, j, y, j);
                     if (maze.get(y)[j].hight >= currTree.hight) {
                         currTree.visible = false;
                         break;
@@ -106,6 +106,67 @@ public class Dec8 {
             }
         }
         return rows;
+    }
+
+    private static void part2() {
+        List<Tree[]> maze = readInput();
+
+        long maxVisibility = 0;
+        int[] visibility = new int[4];
+
+        for (int i = 1; i < maze.size() - 1; i++) {
+            asd: for (int j = 1; j < maze.get(0).length - 1; j++) {
+                Tree currTree = maze.get(i)[j];
+
+                // check left
+                for (int x = j - 1; x >= 0; x--) {
+                    // System.out.printf("Checking %s and comparing %d:%d to %d:%d%n", "LEFT", i, j, i, x);
+                    visibility[0] += 1;
+                    if (maze.get(i)[x].hight >= currTree.hight) {
+                        currTree.visible = false;
+                        break;
+                    }
+                }
+
+                // check right
+                for (int x = j + 1; x < maze.get(0).length; x++) {
+                    // System.out.printf("Checking %s and comparing %d:%d to %d:%d%n", "RIGHT", i, j, i, x);
+                    visibility[1] += 1;
+                    if (maze.get(i)[x].hight >= currTree.hight) {
+                        currTree.visible = false;
+                        break;
+                    }
+                }
+                
+                // check up
+                for (int y = i - 1; y >= 0; y--) {
+                    // System.out.printf("Checking %s and comparing %d:%d to %d:%d%n", "UP", i, j, y, j);
+                    visibility[2] += 1;
+                    if (maze.get(y)[j].hight >= currTree.hight) {
+                        currTree.visible = false;
+                        break;
+                    }
+                }
+                
+                // check down
+                for (int y = i + 1; y < maze.size(); y++) {
+                    // System.out.printf("Checking %s and comparing %d:%d to %d:%d%n", "DOWN", i, j, y, j);
+                    visibility[3] += 1;
+                    if (maze.get(y)[j].hight >= currTree.hight) {
+                        currTree.visible = false;
+                        break;
+                    }
+                }
+
+                long tmpVisibility = visibility[0] * visibility[1] * visibility[2] * visibility[3];
+                Arrays.fill(visibility, 0);
+                if (tmpVisibility > maxVisibility){
+                    maxVisibility = tmpVisibility;
+                }
+            }
+        }
+
+        System.out.println("Max visibility: " + maxVisibility);
     }
 
     static class Tree {
